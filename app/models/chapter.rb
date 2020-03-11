422457190f760223789a74310afa0437655e47d9
Thibaud Maurel <maurel.thibaud@free.fr>
2020-03-11 17:28:05 +0100

    active storage

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,4 +1,5 @@
 class Chapter < ApplicationRecord
   has_many :paragraphs
   belongs_to :part
+  has_many_attached :photos
 end
--- app/models/part.rb
+++ app/models/part.rb
@@ -1,3 +1,4 @@
 class Part < ApplicationRecord
   has_many :chapters
+  has_one_attached :photo
 end
